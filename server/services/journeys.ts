21433c954b81dbf5d005fd0b393ee5978aaf3268
Elias Peteri <elias.peteri@hotmail.com>
2023-01-04 00:21:29 +0200

    rework: Moved business logic into a more fitting place.

--- server/services/journeys.ts
+++ server/services/journeys.ts
@@ -1,9 +1,52 @@
 import { Journey } from 'bike-app-common';
 import JourneyModel from '../models/journey';
+import JourneyBucket from '../models/journeyBucket';
+import { logger } from '../utils';
+import StationService from './stations';
 
 const getAll = async (): Promise<Journey[] | null> => {
   return JourneyModel.find({});
 };
-const JourneyService = { getAll };
+
+const saveMany = async (fileDataParsed: Journey[] | undefined) => {
+  fileDataParsed &&
+    fileDataParsed.forEach((row: Journey) => {
+      const startDate = row.departureTime;
+      const endDate = row.returnTime;
+
+      // Find a matching bucket document
+      JourneyBucket.findOneAndUpdate(
+        {
+          startDate: {
+            $gte: startDate.slice(0, 10)
+          },
+          endDate: { $lt: endDate.slice(0, 10) },
+          count: { $lt: 1000 }
+        },
+        {
+          startDate: startDate,
+          endDate: endDate,
+          $push: { journeys: row },
+          $inc: { count: 1 }
+        },
+        { upsert: true },
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        (error, _result) => {
+          if (error) {
+            logger.error(error);
+          }
+        }
+      );
+      StationService.saveMany([
+        {
+          stationId: row.departureStationId,
+          stationName: row.departureStationName
+        },
+        { stationId: row.returnStationId, stationName: row.returnStationName }
+      ]);
+    });
+};
+
+const JourneyService = { getAll, saveMany };
 
 export default JourneyService;
--- server/services/upload.ts
+++ server/services/upload.ts
@@ -1,8 +1,6 @@
-import JourneyBucket from '../models/journeyBucket';
+import JourneyService from './journeys';
 
-import { logger } from '../utils';
-
-const saveMany = async (fileData: string | undefined) => {
+const uploadMany = async (fileData: string | undefined) => {
   try {
     const fileDataParsed = fileData
       ?.split('\n')
@@ -12,51 +10,16 @@ const saveMany = async (fileData: string | undefined) => {
         return {
           departureTime: row[0],
           returnTime: row[1],
-          departureStationId: row[2],
+          departureStationId: parseInt(row[2]),
           departureStationName: row[3],
-          returnStationId: row[4],
+          returnStationId: parseInt(row[4]),
           returnStationName: row[5],
           distanceCoveredInMeters: parseInt(row[6]),
           durationSeconds: parseInt(row[7])
         };
       })
       .filter((row) => row.departureTime !== '');
-    fileDataParsed &&
-      fileDataParsed.forEach((row) => {
-        const startDate = row.departureTime;
-        const endDate = row.returnTime;
-
-        // logger.table({
-        //   depId: row.departureStationId,
-        //   startDate: startDate.slice(0, 10),
-        //   endDate: endDate.slice(0, 10),
-        //   retId: row.returnStationId
-        // });
-
-        // Find a matching bucket document
-        JourneyBucket.findOneAndUpdate(
-          {
-            startDate: {
-              $gte: startDate.slice(0, 10)
-            },
-            endDate: { $lt: endDate.slice(0, 10) },
-            count: { $lt: 1000 }
-          },
-          {
-            startDate: startDate,
-            endDate: endDate,
-            $push: { journeys: row },
-            $inc: { count: 1 }
-          },
-          { upsert: true },
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          (error, _result) => {
-            if (error) {
-              logger.error(error);
-            }
-          }
-        );
-      });
+    JourneyService.saveMany(fileDataParsed);
     return {
       success: true,
       message: 'successful entry'
@@ -68,6 +31,6 @@ const saveMany = async (fileData: string | undefined) => {
   }
 };
 
-const UploadService = { saveMany };
+const UploadService = { uploadMany };
 
 export default UploadService;

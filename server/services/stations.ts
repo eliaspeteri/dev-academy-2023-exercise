7d88e0dcc2b797936c8267f909290cd823998aba
Elias Peteri <elias.peteri@hotmail.com>
2023-01-04 00:20:31 +0200

    feat: Created a service to add stations as well.

--- server/services/stations.ts
+++ server/services/stations.ts
@@ -4,6 +4,24 @@ import StationModel from '../models/station';
 const getAll = async (): Promise<Station[] | null> => {
   return StationModel.find({});
 };
-const StationService = { getAll };
+
+const saveMany = async (stations: Station[]) => {
+  await StationModel.insertMany(stations);
+};
+
+const saveOne = async (station: Station) => {
+  const existingStation = await StationModel.findOne({
+    stationId: station.stationId
+  });
+
+  if (!existingStation) {
+    const newStation = new StationModel(station);
+    await newStation.save();
+    return newStation;
+  }
+  return 'Station already exists.';
+};
+
+const StationService = { getAll, saveMany, saveOne };
 
 export default StationService;

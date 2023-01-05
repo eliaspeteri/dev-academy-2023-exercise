7e9edb76ce951ae9dfc1d16ba37addecaa3f0491
Elias Peteri <elias.peteri@hotmail.com>
2023-01-05 13:07:03 +0200

    fix: Stronger typing and updating names.

--- server/services/upload.ts
+++ server/services/upload.ts
@@ -1,8 +1,9 @@
+import { Journey } from 'bike-app-common';
 import JourneyService from './journeys';
 
-const uploadMany = async (fileData: string | undefined) => {
+const uploadOne = async (fileData: string | undefined) => {
   try {
-    const fileDataParsed = fileData
+    const fileDataParsed: Journey[] | undefined = fileData
       ?.split('\n')
       .map((row) => row.split(','))
       .splice(1, fileData.length - 1)
@@ -23,7 +24,6 @@ const uploadMany = async (fileData: string | undefined) => {
     return {
       success: true,
       message: 'successful entry'
-      /*data: fileDataParsed*/
     };
   } catch (error) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -31,6 +31,6 @@ const uploadMany = async (fileData: string | undefined) => {
   }
 };
 
-const UploadService = { uploadMany };
+const UploadService = { uploadOne };
 
 export default UploadService;
